42aedd2224461b02cad2e93c19a4a1cee8f73789
Dawid Dobrowolski <dawid89dobrowolski@gmail.com>
2021-11-03 22:28:12 +0100

    wait unitl networkidle

--- support/UI/Login.ts
+++ support/UI/Login.ts
@@ -4,13 +4,13 @@ export class Login {
     private static username: string = 'dawid89dobrowolski@brave-wolf-qm0gmg.com';
     private static password: string = 'Qwerty123';
     static async toSfdc (page: Page): Promise<void> {
-        await page.goto(`https://login.salesforce.com?un=${this.username}&pw=${this.password}`);
+        await page.goto(`https://login.salesforce.com?un=${this.username}&pw=${this.password}`, {waitUntil: "networkidle"});
         // await page.waitForEvent("framenavigated");
-        // if (await page.url.toString().includes("LoginInterstitial")){
-        //     await page.click("//*[contains(@value, 'Continue')]");
-        // }
-        try {
-            await page.click("//*[contains(@value, 'Continue')]", {timeout: 5000});
-        } catch(e){}
+        if (await page.url.toString().includes("LoginInterstitial")){
+            await page.click("//*[contains(@value, 'Continue')]");
+        }
+        // try {
+        //     await page.click("//*[contains(@value, 'Continue')]", {timeout: 5000});
+        // } catch(e){}
     }
 }
