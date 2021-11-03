834b0822c78378e0eeccd3218a46c7159d645cdb
Dawid Dobrowolski <dawid89dobrowolski@gmail.com>
2021-11-03 23:12:01 +0100

    fixed login

--- support/UI/Login.ts
+++ support/UI/Login.ts
@@ -1,12 +1,15 @@
 import { expect, Page } from "@playwright/test";
 
 export class Login {
-    private static username: string = 'dawid89dobrowolski@brave-wolf-qm0gmg.com';
-    private static password: string = 'Qwerty123';
+    private static username: string = 'test-atbow15lsqwf@example.com';
+    private static password: string = 'apQ0h-skeakdb';
     static async toSfdc (page: Page): Promise<void> {
-        await page.goto(`https://login.salesforce.com?un=${this.username}&pw=${this.password}`, {waitUntil: "domcontentloaded"});
-        if (page.url.toString().includes("LoginInterstitial")){
+        await page.goto(`https://CS189.salesforce.com?un=${this.username}&pw=${this.password}`);
+        if (await page.url().toString().includes("LoginInterstitial")){
             await page.click("//*[contains(@value, 'Continue')]");
+        }
+        if (await page.url().toString().includes("AddPhoneNumber")){
+            await page.click("//*[contains(text(), 'Want to Register My Phone')]");
         }
     }
 }
