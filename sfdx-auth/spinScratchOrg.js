dffc2e8a4150c97a5d651abf373e5443a8ec71c0
Dawid Dobrowolski <dawid89dobrowolski@gmail.com>
2021-11-06 18:06:29 +0100

    fix error handling once again

--- sfdx-auth/spinScratchOrg.js
+++ sfdx-auth/spinScratchOrg.js
@@ -7,8 +7,11 @@ const repo = 'salesforce-test-org';
 const sfdcModuleRepoLink = `"https://github.com/dawiddiwad/${repo}.git"`;
 
 async function prepareOrg() {
-    await fs.writeFile('sfdx-auth/auth.json', process.argv[2], err => {
-        console.log(err ? err : 'auth.json saved');
+    await fs.writeFile('sfdx-auth/auth.json', process.argv[2], error => {
+        if (error){
+            throw new Error(`unable to save auth.josn due to:\n${error.message}`);
+        }
+        console.log('auth.json saved');
     })
     const gitClone = exec(`git clone --branch ${branch} ${sfdcModuleRepoLink}`,
         (err, stdout, sterr) => {
@@ -20,66 +23,67 @@ async function prepareOrg() {
 };
 
 async function spinOrg() {
-    try {
-        process.chdir('./salesforce-test-org');
-        const auth = await sfdx.auth.sfdxurl.store({
-            _quiet: false,
-            sfdxurlfile: '../sfdx-auth/auth.json',
-            setdefaultdevhubusername: true,
-            setdefaultusername: true
+    process.chdir('./salesforce-test-org');
+    const auth = await sfdx.auth.sfdxurl.store({
+        _quiet: false,
+        sfdxurlfile: '../sfdx-auth/auth.json',
+        setdefaultdevhubusername: true,
+        setdefaultusername: true
+    });
+
+    const orgList = await sfdx.force.org.list();
+    let targetScratchOrg = null;
+
+    if (orgList.scratchOrgs.length !== 0) {
+        targetScratchOrg = orgList.scratchOrgs[0];
+        orgList.scratchOrgs.forEach(scratchOrg => {
+            if (scratchOrg.alias === repo) {
+                targetScratchOrg = scratchOrg;
+            }
         });
+    }
 
-        const orgList = await sfdx.force.org.list();
-        let targetScratchOrg = null;
+    targetScratchOrg ? targetScratchOrg : await sfdx.force.org.create({
+        _quiet: false,
+        _rejectOnError: true,
+        definitionfile: 'config/project-scratch-def.json',
+        targetdevhubusername: auth.username,
+        durationdays: 1,
+        setalias: repo
+    });
 
-        if (orgList.scratchOrgs.length !== 0) {
-            targetScratchOrg = orgList.scratchOrgs[0];
-            orgList.scratchOrgs.forEach(scratchOrg => {
-                if (scratchOrg.alias === repo) {
-                    targetScratchOrg = scratchOrg;
-                }
-            });
-        }
-
-        targetScratchOrg ? targetScratchOrg : await sfdx.force.org.create({
+    if (targetScratchOrg) {
+        await sfdx.force.source.push({
             _quiet: false,
-            _rejectOnError: true,
-            definitionfile: 'config/project-scratch-def.json',
-            targetdevhubusername: auth.username,
-            durationdays: 1,
-            setalias: repo
+            forceoverwrite: true,
+            targetusername: targetScratchOrg.username
         });
-
-        if (targetScratchOrg) {
-            await sfdx.force.source.push({
+        if (!targetScratchOrg.password) {
+            targetScratchOrg.password = await sfdx.force.user.passwordGenerate({
                 _quiet: false,
-                forceoverwrite: true,
                 targetusername: targetScratchOrg.username
-            });
-            if (!targetScratchOrg.password) {
-                targetScratchOrg.password = await sfdx.force.user.passwordGenerate({
-                    _quiet: false,
-                    targetusername: targetScratchOrg.username
-                }).password;
-            }
-            const credentials = {
-                loginUrl: targetScratchOrg.loginUrl,
-                username: targetScratchOrg.username,
-                password: targetScratchOrg.password,
-                baseUrl: targetScratchOrg.instanceUrl
-            }
-            await fs.writeFile('sfdx-auth/credentials.json', JSON.stringify(credentials), err => {
-                console.log(err ? err : 'credentials created');
-            })
-        } else {
-            throw new Error("scracth orgs not available");
+            }).password;
         }
-    } catch (error) {
-        throw new Error(`unable to spinup scratch orgs due to:\n${JSON.stringify(error)}`);
+        const credentials = {
+            loginUrl: targetScratchOrg.loginUrl,
+            username: targetScratchOrg.username,
+            password: targetScratchOrg.password,
+            baseUrl: targetScratchOrg.instanceUrl
+        }
+        await fs.writeFile('sfdx-auth/credentials.json', JSON.stringify(credentials), err => {
+            console.log(err ? err : 'credentials created');
+        })
+    } else {
+        throw new Error("scracth orgs not available");
     }
 }
 
-prepareOrg();
+try {
+    prepareOrg();
+} catch (error) {
+    throw new Error(`unable to spinup scratch orgs due to:\n${error.message}`);
+}
+
 
 
 
