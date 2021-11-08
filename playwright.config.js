4bb995c6e6c513d843c3f5cd619347d1603cbd0a
Dawid Dobrowolski <dawid89dobrowolski@gmail.com>
2021-11-08 08:53:28 +0100

    debug

--- playwright.config.js
+++ playwright.config.js
@@ -3,7 +3,7 @@
 /** @type {import('@playwright/test').PlaywrightTestConfig} */
 const config = {
   reporter: [['html', { outputFolder: 'playwright-report' }], ['list']],
-  repeatEach: 10,
+  repeatEach: 1,
   retries: 2,
   workers: 2,
   use: {
