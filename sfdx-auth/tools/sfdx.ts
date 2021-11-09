8d1b445f1544fe593e3e05c8d19bf8e84b8115dd
Dawid Dobrowolski <dawid89dobrowolski@gmail.com>
2021-11-09 10:06:23 +0100

    refactor sfdx.ts

--- sfdx-auth/tools/sfdx.ts
+++ sfdx-auth/tools/sfdx.ts
@@ -1,15 +1,15 @@
-import {exec} from 'child_process';
+import { exec } from 'child_process';
 
 interface SdfxParameters {
-    cmd: string,
-    f: Array<string>,
-    log?: boolean
+    cmd :   string,
+    f   :   Array<string>,
+    log?:   boolean
 }
 
 export class SFDX {
     private path: string;
 
-    constructor (path: string){
+    constructor(path: string) {
         this.path = path;
     }
 
@@ -17,21 +17,23 @@ export class SFDX {
         let params: string = '';
         paramsList.forEach((param) => {
             params += `${param} `;
-        })
+        });
         return params;
     }
 
-    public exec({cmd, f: flags, log}: SdfxParameters): Promise<string> {
+    public exec({ cmd, f: flags, log }: SdfxParameters): Promise<string> {
         return new Promise<string>((sfdxResponse, sfdxFailure) => {
             exec(`${this.path} ${cmd} ${this.pass(flags)}`, (error, stdout, stderr) => {
                 if (error) {
-                    sfdxFailure(`sfdx execution failed on:\n${error} ${stderr}`);
-                } else {
-                    log ? console.log(`sfdx response:\n${stdout}`) : null;
-                    sfdxResponse(stdout)
+                    sfdxFailure(`SFDX command failed with exit code: ${error.code} caused by:\n${error}`);
+                } else if (stderr) {
+                    sfdxFailure(`SFDX command failed on:\n${stderr}`);
                 }
-            })
-        })
+                else {
+                    log ? console.log(`SFDX command response:\n${stdout}`) : null;
+                    sfdxResponse(stdout);
+                }
+            });
+        });
     }
 }
-
