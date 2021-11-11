77140f64adc5f1be503e045e3d0a04d8181adab8
Dawid Dobrowolski <dawid89dobrowolski@gmail.com>
2021-11-11 21:03:26 +0100

    debug

--- sfdx-auth/spinScratchOrg.ts
+++ sfdx-auth/spinScratchOrg.ts
@@ -7,9 +7,9 @@ const sfdx = Client.createUsingPath('sfdx');
         console.log(warning.stack);
     });
     console.log('preparing scratch org for testing:');
-    const scratchOrg = new ScratchPreparator('sfdx', {url: process.argv[2]}, 'salesforce-test-org', 'develop');
+    const scratchOrg = new ScratchPreparator('sfdx', { url: process.argv[2] }, 'salesforce-test-org', 'develop');
     try {
-        await scratchOrg.Ready.then( async (org) => {
+        await scratchOrg.Ready.then(async (org) => {
             await org.push();
             await org.generatePassword();
             await org.credentialsFile(await org.fetchCredentials());
