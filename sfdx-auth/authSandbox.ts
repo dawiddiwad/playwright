95be6d92f8c54e4c10ead2a6a4fc895c9d259482
Dawid Dobrowolski <dawid89dobrowolski@gmail.com>
2021-11-12 17:12:41 +0100

    debug

--- sfdx-auth/authSandbox.ts
+++ sfdx-auth/authSandbox.ts
@@ -1,57 +1,17 @@
-import { writeFile } from 'fs/promises';
-import { Client } from 'sfdx-js';
-const sfdx = Client.createUsingPath('sfdx');
+import { SandboxPreparator, SANDBOX_CREDENTIALS } from "./tools/SandboxPreparator";
 
-async function readAuth() {
-    console.log('[step 1/5] fetching auth token...');
-    await writeFile('./sfdx-auth/auth.json',`{"sfdxAuthUrl": "${process.argv[2]}"}`);
-}
-
-async function authorize() {
-    console.log('[step 2/5] authorizing org...');
-    await sfdx.auth.sfdxurlStore({
-        sfdxurlfile: './sfdx-auth/auth.json',
-        setdefaultdevhubusername: true,
-        setdefaultusername: true
-    });
-}
-
-async function generateCredentials() {
-    console.log('[step 3/5] fetching sanboxes...');
-    const orgList: any = await sfdx.org.list({
-        noprompt: true,
-        all: true,
-        clean: true,
-        json: true
-    });
-
-    console.log('[step 4/5] fetching credentials data...');
-    const targetOrg = orgList.result.nonScratchOrgs[0];
-    const credentials = {
-        loginUrl: targetOrg.loginUrl,
-        username: targetOrg.username,
-        password: process.argv[3],
-        baseUrl: targetOrg.instanceUrl
-    }
-
-    console.log('[step 5/5] writing credentials file...');
-    await writeFile('./sfdx-auth/credentials.json', JSON.stringify(credentials));
-}
-
+const token = process.argv[2];
+const password = process.argv[3];
+const sandbox = new SandboxPreparator('sfdx', { url: token }, 'salesforce-test-org', 'develop');
 (async () => {
-    console.log('generating sandbox credentials in 5 steps:')
-    try{
-        await readAuth();
-        await authorize();
-        await generateCredentials();
-    } catch (errors) {
-        console.error(`unable to generate sandbox credentials due to:\n${errors}`); 
+    try {
+        await sandbox.Ready.then(async (org) => {
+            let data: SANDBOX_CREDENTIALS = await org.fetchCredentials();
+            data.password = password;
+            await org.credentialsFile(data);
+        })
+    } catch (error) {
+        console.error(`unable to prepare sandbox due to:\n${error}`);
         process.exit(1);
     }
-})();
-
-
-
-
-
-
+})()
--- support/UI/SFDC.ts
+++ support/UI/SFDC.ts
@@ -29,7 +29,7 @@ export class SFDC {
             await page.click(Modal.skipPhoneRegistrationLink);
         }
         if (this.isOn(page, LoginInteruption.ClassicContext)) {
-            await page.goto(this.getBaseUrlForLEX());
+            await page.goto(this.getBaseUrlForLEX(), {waitUntil: 'networkidle'});
         }
     }
 
@@ -47,11 +47,11 @@ export class SFDC {
     }
 
     public static async login(page: Page): Promise<void> {
-        await page.goto(`${this.loginUrl}?un=${this.username}&pw=${this.password}`);
+        await page.goto(`${this.loginUrl}?un=${this.username}&pw=${this.password}`, {waitUntil: 'networkidle'});
         await this.checkInteruptions(page);
     }
 
     public static async logout(page: Page): Promise<void> {
-        await page.goto(`${this.baseUrl}/secur/logout.jsp`);
+        await page.goto(`${this.baseUrl}/secur/logout.jsp`, {waitUntil: 'networkidle'});
     }
 }
