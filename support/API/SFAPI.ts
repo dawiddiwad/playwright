ec53ed3dc091180cd66f15f2d0a33f8b67c16e7a
Dawid Dobrowolski <dawid89dobrowolski@gmail.com>
2021-11-17 21:00:06 +0100

    WIP

--- support/API/SFAPI.ts
+++ support/API/SFAPI.ts
@@ -1,4 +1,4 @@
-import { Connection, QueryResult, RecordResult, UserInfo } from "jsforce";
+import { Connection, ErrorResult, QueryResult, Record, RecordResult, RecordStream, SalesforceId, SObject, SuccessResult, UserInfo } from "jsforce";
 
 interface SFDC_CREDENTIALS {
     username: string,
@@ -16,30 +16,68 @@ export class SFDCapi {
             try {
                 this.userInfo = await this.conn.login(credentials.username, credentials.password);
                 connected(this);
-            } catch(e) {
-                console.error(`unable to initialize SFDC API due to:\n${e}`);
-                failure(e);
+            } catch(error) {
+                console.error(`unable to initialize SFDC API due to:\n${error}`);
+                failure(error);
             }
         })
     }
 
-    public async create(sobject: string, data: Object | Array<Object>): Promise<RecordResult | RecordResult[]> {
+    private checkForErrors(results: RecordResult[]): SuccessResult[] {
+        let errors: string[] = [];
+        results.forEach((result) => {
+            if (!result.success){
+                (result as ErrorResult).errors
+                    .forEach((error) => errors.push(error));
+            }
+        })
+        if (errors.length > 0) {
+            throw new Error(`some issues on creating ${JSON.stringify(errors)}`);
+        } else return results as SuccessResult[];
+    }
+
+    public async create(sobject: string, data: Object | Array<Object>): Promise<SuccessResult | SuccessResult[]> {
         try {
             console.log(`creating new ${sobject} ...`);
-            const result: RecordResult | RecordResult[] = await this.conn.create(sobject, data);
-            console.log(`created ${sobject} :\n${JSON.stringify(result)}`);
-            return result;
-        } catch (e) {
-            console.error(`unable to create ${sobject} due to:\n${e}`);
+            const results = await this.conn.create(sobject, data, {allOrNone: true});
+            if (results instanceof Array){
+                return this.checkForErrors(results);
+            } else {
+                return results as SuccessResult;
+            }
+        } catch (error) {
+            console.error(`unable to create ${sobject} due to:\n${(error as Error).stack}`);
             process.exit(1);
+        }
+    }
+
+    public async delete(sobject: string, id: SalesforceId | string[]): Promise<RecordResult | RecordResult[]>{
+        try {
+            console.log(`deleting ${sobject} records ${id} ...`);
+            const result = await this.conn.delete(sobject, id);
+            console.log(`deleted ${sobject} :\n${JSON.stringify(result)}`);
+            return result;
+        } catch (error) {
+            console.error(`unable to delete ${sobject} due to:\n${error}`);
+            process.exit(1);
+        }
+    }
+
+    public async read(sobject: string, id: SalesforceId | string[]): Promise<Record | Record[]>{
+        try {
+            console.log(`reading ${sobject} data of ${id} ...`);
+            const result = await this.conn.retrieve(sobject, id);
+            console.log(result);
+        } catch (error) {
+            
         }
     }
 
     public async query(soql: string): Promise<QueryResult<unknown>>{
         try {
             return await this.conn.query(soql);
-        } catch (e) {
-            console.error(`unable to execute soql:\n${soql}\ndue to:\n${e}`);
+        } catch (error) {
+            console.error(`unable to execute soql:\n${soql}\ndue to:\n${error}`);
             process.exit(1);
         }
     }
@@ -48,7 +86,8 @@ export class SFDCapi {
 (async ()=> {
     new SFDCapi({username: 'dawid89dobrowolski@brave-wolf-qm0gmg.com', password: '%Zaamdkjop9rn'})
         .Ready.then(async (api) => {
-            await api.create('Account', {name: 'api'});
+            const acc = await api.create('Account', [{name: 'api2'}, {yolo: 'api2'}]);
+            console.log(acc);
             console.log((await api.query('select id from Account')).totalSize);
         })
 })()
